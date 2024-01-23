d833d384dbb24483fc849df958a2bede3672267f
ollatomiwa <ajaomoshood@gmail.com>
2024-01-23 21:18:35 +0100

    enable multiple selection added

--- vite-react-app/src/components/accordions/index.jsx
+++ vite-react-app/src/components/accordions/index.jsx
@@ -2,30 +2,50 @@
 /* single and multiple selection */
 import { useState } from "react";
 import data from "./data";
-import './styles.css';
+import "./styles.css";
 
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
+  const [enableMultipleSelection, setEnableMultipleSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
   function handleSinglSelection(getCurrentId) {
     setSelected(getCurrentId === selected ? null : getCurrentId);
   }
+  function handleMultipleSelection(getCurrentId) {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+
+    setMultiple(cpyMultiple);
+  }
 
   return (
     <div className="wrapper">
+      <button
+        onClick={() => setEnableMultipleSelection(!enableMultipleSelection)}
+      >
+        Enable Multiple Selection
+      </button>
       <div className="accordion">
         {data && data.length > 0 ? (
           // eslint-disable-next-line react/jsx-key
           data.map((dataItem) => (
             <div className="item">
               <div
-                onClick={() => handleSinglSelection(dataItem.id)}
+                onClick={
+                  enableMultipleSelection
+                    ? () => handleMultipleSelection(dataItem.id)
+                    : () => handleSinglSelection(dataItem.id)
+                }
                 className="title"
               >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-              {selected === dataItem.id ? (
+              {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
                 <div className="content">{dataItem.answer}</div>
               ) : null}
             </div>
